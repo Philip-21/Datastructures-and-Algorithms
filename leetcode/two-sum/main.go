1065b0becda35468fbe5c535773328cf67fc0707
Philip-21 <philipuzomaobiora@gmail.com>
2023-03-06 06:49:41 +0100

    commenting more

--- leetcode/two-sum/main.go
+++ leetcode/two-sum/main.go
@@ -9,8 +9,27 @@ two numbers such that they add up to target.
 func twoSum(nums []int, target int) []int {
 	result := []int{0, 0}
 	//creating a hash map to store numbers
-	m := make(map[int]int, len(nums))
+	/*
+			the hash map makes this algorithm possible by allowing constant-time lookups of
+			 the complement of each element in the nums slice.
+			 The algorithm iterates over each element in the nums slice and checks if the complement of the current element exists in the hash map.
+			  If the complement exists in the hash map, the function has found a pair of elements that add up to the target,
+		      and the function returns the indices of these two elements
+	*/
+	/*
+		THIS IS THE BIG O NOTATION ALGORITHM
+		Using a hash map to store the elements of the nums slice allows the function to achieve a time complexity of O(n) because
+		the lookup and insertion operations into the hash map take constant time.
+		 This is much faster than the O(n^2) time complexity of a brute-force algorithm that compares each pair of elements in the nums slice.
+	*/
+	m := make(map[int]int)
 
+	/*
+		 iterates over each element in the nums slice and calculates
+		 the complement of the current element that would add up to the target
+		The complement is then checked if it exists in the hash map. If the complement exists,
+		 it means that the function has found a pair of elements that add up to the target
+	*/
 	for idx, val := range nums {
 		i, ok := m[target-val]
 		if ok {
@@ -23,6 +42,7 @@ func twoSum(nums []int, target int) []int {
 			fmt.Println([]int{i, idx})
 			return []int{i, idx}
 		} else {
+			//adds te current index to the hash map
 			m[val] = idx
 		}
 
